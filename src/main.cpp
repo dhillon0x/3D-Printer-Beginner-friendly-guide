15c33e5b56836efd1b19da6c21a4f4fc54f3bc52
Harpinder Singh <25bas10006@cuchd.in>
2026-09-08 17:15:16 +0530

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,31 @@
+#include <Arduino.h>
+#include "../include/Configuration.h"
+#include "GCodeParser.hpp"
+void init_stepper_timer();
 
+GCodeParser parser;
+
+void setup() {
+    parser.init();
+    pinMode(X_STEP_PIN, OUTPUT);
+    pinMode(X_DIR_PIN, OUTPUT);
+    pinMode(X_ENABLE_PIN, OUTPUT);
+    
+    pinMode(Y_STEP_PIN, OUTPUT);
+    pinMode(Y_DIR_PIN, OUTPUT);
+    pinMode(Y_ENABLE_PIN, OUTPUT);
+    
+    pinMode(Z_STEP_PIN, OUTPUT);
+    pinMode(Z_DIR_PIN, OUTPUT);
+    pinMode(Z_ENABLE_PIN, OUTPUT);
+  
+    digitalWrite(X_ENABLE_PIN, LOW);
+    digitalWrite(Y_ENABLE_PIN, LOW);
+    digitalWrite(Z_ENABLE_PIN, LOW);
+
+    init_stepper_timer();
+}
+
+void loop() {
+    parser.poll();
+}
